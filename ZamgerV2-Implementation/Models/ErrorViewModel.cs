de91adb5e149a3f1de28a2a7f27d1fec85f025e3
Huso Hamzić <hhamzic1@etf.unsa.ba>
2020-05-06 19:02:53 +0200

    Urađen login screen(implementacija)

---
+++ ZamgerV2-Implementation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ZamgerV2_Implementation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

f7b0b7f31a7e4a4dbd45f2a794fbb7147ccb7155
Huso Hamzić <hhamzic1@etf.unsa.ba>
2020-05-12 23:42:08 +0200

    studentska update

--- ZamgerV2-Implementation/Controllers/StudentskaController.cs
+++ ZamgerV2-Implementation/Controllers/StudentskaController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ZamgerV2_Implementation.Controllers
@@ -22,8 +23,22 @@ namespace ZamgerV2_Implementation.Controllers
 
         [Route("/studentska/svi-studenti/kreiraj-studenta")]
         [Route("/studentska/kreiraj-studenta")]
+        [HttpGet]
         public IActionResult KreirajStudenta()
         {
+            return View();
+        }
+
+
+        [Route("/studentska/svi-studenti/kreiraj-studenta")]
+        [Route("/studentska/kreiraj-studenta")]
+        [HttpPost]
+        public IActionResult KreirajStudenta(IFormCollection forma)
+        {
+            foreach (String kljuc in forma.Keys)
+            {
+                Response.WriteAsync("kljuc: " + kljuc + "  -   vrijednost:   " + forma[kljuc] + " --- ");
+            }
             return View();
         }
 
@@ -36,7 +51,31 @@ namespace ZamgerV2_Implementation.Controllers
 
         [Route("/studentska/svo-nastavno-osoblje/kreiraj-nastavno-osoblje")]
         [Route("/studentska/kreiraj-nastavno-osoblje")]
+        [HttpGet]
         public IActionResult KreirajNastavnoOsoblje()
+        {
+            return View();
+        }
+
+        [Route("/studentska/svo-nastavno-osoblje/kreiraj-nastavno-osoblje")]
+        [Route("/studentska/kreiraj-nastavno-osoblje")]
+        [HttpPost]
+        public IActionResult KreirajNastavnoOsoblje(IFormCollection forma)
+        {
+            foreach(String kljuc in forma.Keys){
+                Response.WriteAsync("kljuc: " + kljuc + "  -   vrijednost:   " + forma[kljuc]+" --- ");
+            }
+            return View();
+        }
+
+        [Route("/studentska/svi-studenti-list")]
+        public IActionResult AllStudentsList()
+        {
+            return View();
+        }
+
+        [Route("/studentska/svo-nastavno-osoblje-list")]
+        public IActionResult AllTeachersList()
         {
             return View();
         }
